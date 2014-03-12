2c417b3d229810507dddbfbcb344bb59ff4ed512
rbeko <renatbek@gmail.com>
2014-03-12 07:52:58 -0700

    retab

--- tryNames.py
+++ tryNames.py
@@ -10,46 +10,46 @@ disruptionsAll = 0
 prefix = "r"
 
 def tryName(s):
-	global c
-	global disruptions
-	global disruptionsAll
-	r = urllib2.urlopen(baseUrl+s)
-	#r = requests.get(baseUrl+s)
-	d = json.load(r)
-	b = d["valid"]
-	if b:
-		print "\nName: " + s + " is available\n"
-	c += 1
-	
+  global c
+  global disruptions
+  global disruptionsAll
+  r = urllib2.urlopen(baseUrl+s)
+  #r = requests.get(baseUrl+s)
+  d = json.load(r)
+  b = d["valid"]
+  if b:
+    print "\nName: " + s + " is available\n"
+  c += 1
+  
 def checkName(p):
-	global disruptions
-	global disruptionsAll
-	b = True
-	d = 0
-	while(b):
-		try:
-			sys.stdout.write('\r{0} tested [d: {1}, da: {2}] (checking)               '.format(c,disruptions, disruptionsAll))
-			tryName(p)
-			b = False
-		except:
-			d += 1
-			sys.stdout.write('\r{0} tested [d: {1}, da: {2}] (waiting before retry)'.format(c,disruptions, disruptionsAll))
-			sys.stdout.flush()
-			time.sleep(5*pow(2,d))
-	if d > 0:
-		disruptions += 1			
-		disruptionsAll += d
+  global disruptions
+  global disruptionsAll
+  b = True
+  d = 0
+  while(b):
+    try:
+      sys.stdout.write('\r{0} tested [d: {1}, da: {2}] (checking)               '.format(c,disruptions, disruptionsAll))
+      tryName(p)
+      b = False
+    except:
+      d += 1
+      sys.stdout.write('\r{0} tested [d: {1}, da: {2}] (waiting before retry)'.format(c,disruptions, disruptionsAll))
+      sys.stdout.flush()
+      time.sleep(5*pow(2,d))
+  if d > 0:
+    disruptions += 1      
+    disruptionsAll += d
 
 def comb(list,n,p):
-	if n < 1:
-		checkName(p)
-	else:
-		for i in list:
-			comb(list,n-1,p+i)
-		
+  if n < 1:
+    checkName(p)
+  else:
+    for i in list:
+      comb(list,n-1,p+i)
+    
 
 if len(sys.argv) > 1:
-	prefix = sys.argv[1][:cn]
+  prefix = sys.argv[1][:cn]
 
 print datetime.now()
 print "\nTrying " + str(cn) + "-letter names starting with " + prefix + "\n"
